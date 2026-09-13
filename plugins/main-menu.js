6e0238e5a1cd01bd4f9a0772a565d9c770d29ba6
AHMAD HASSAN <ahmadtechx8@gmail.com>
2026-09-13 10:11:19 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -77,7 +77,7 @@ ${menuSections}
 > *✨ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀʜᴍᴀᴅ ʜᴀssᴀɴ ✨*`;
 
         // Image URL Selection
-        let imageToUse = "https://files.catbox.moe/w3y3eg.jpg";
+        let imageToUse = "https://files.catbox.moe/ptvl03.jpg";
 
         // 1. Menu Image Send with Caption
         await conn.sendMessage(from, { 
